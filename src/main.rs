76ee8cf4df2ca8c6fcd6e9c403be4653e7b184e8
leonlux <leonlux@icloud.com>
2023-11-06 15:39:09 +0100

    add simple example to test if raylib works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let ( mut rl, thread) = raylib::init()
+        .size(800, 600)
+        .title("algoviz")
+        .build();
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::WHITE);
+        d.draw_text("Hello, Raylib", 12, 12, 20, Color::BLACK);
+    }
 }
